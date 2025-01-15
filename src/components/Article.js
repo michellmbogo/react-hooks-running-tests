24893536d6fcb7e04265c2de43925f8e14c119de
Michell Wambui Mbogo <mishie254@gmail.com>
2025-01-15 21:37:11 +0300

    react hook lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,6 @@
-import React from "react";
-
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
-}
+// src/components/Article.js
+const Article = () => {
+  return <div>please pass this test</div>;
+};
 
 export default Article;
